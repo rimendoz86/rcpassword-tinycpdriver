85cb8fe6d415ea19b05c990f640619951b4c506d
Ricardo Mendoza <rimendoz86@gmail.com>
2020-07-21 23:29:11 -0700

    only used for initial development

--- tinycp.test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define("PASSWORD_SUCCESS", "Success");
-define("PASSWORD_ERROR", "Error");
-require("./tinycp.php");
-$driver = new rcube_tinycp_password();
-echo $driver->save("unusedpass","applesauce","test@rickymendoza.dev");
-?>
